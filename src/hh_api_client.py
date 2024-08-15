99db3b45d23ad3c7372f6ba0cd3ec64133fc2874
georgii <goqwertys@gmail.com>
2024-08-16 01:18:16 +0400

    class HHAPIClient added

---
+++ src/hh_api_client.py
@@ -0,0 +1,20 @@
+import requests
+
+from src.api_client import APIClient
+
+
+class HHAPIClient(APIClient):
+    def __init__(self, file_worker):
+        self.url = ''
+        self.headers = {'User-Agent': 'HH-User-Agent'}
+        self.params = {'text': '', 'page': 0, 'per_page': 100}
+        self.vacancies = []
+        super().__init__(file_worker)
+
+    def load_vacancies(self, keyword):
+        self.params['text'] = keyword
+        while self.params.get('page') != 20:
+            response = requests.get(self.url, headers=self.headers, params=self.params)
+            vacancies = response.json()['items']
+            self.vacancies.extend(vacancies)
+            self.params['page'] += 1
