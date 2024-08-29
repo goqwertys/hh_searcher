8d1aba0578c01959bcaf907ff28e7a9d627ba156
georgii <goqwertys@gmail.com>
2024-08-30 02:59:19 +0400

    test_delete_vacancy added

--- tests/test_json_saver.py
+++ tests/test_json_saver.py
@@ -24,9 +24,23 @@ def test_get_vacancies(vacancy_1, vacancy_2, json_saver):
     expected_container = VacancyContainer([vacancy_1, vacancy_2])
 
     # DEBUG PRINT
-    print("expected container:")
-    print(expected_container)
-    print("result container:")
-    print(result_container)
+    # print("expected container:")
+    # print(expected_container)
+    # print("result container:")
+    # print(result_container)
+
+    assert result_container == expected_container
+
+
+def test_delete_vacancy(vacancy_1, vacancy_2, vacancy_3, json_saver):
+    json_saver.add_vacancy(vacancy_1)
+    json_saver.add_vacancy(vacancy_2)
+    json_saver.add_vacancy(vacancy_3)
+
+    json_saver.delete_vacancy(vacancy_3)
+
+    expected_container = VacancyContainer([vacancy_1, vacancy_2])
+
+    result_container = json_saver.get_vacancies()
 
     assert result_container == expected_container
